3eaa18b1ddc7c0825f7c3bf5eec5c2ae95a5f3bd
vedantfire00 <83462470+vedantfire00@users.noreply.github.com>
2021-07-29 20:24:50 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+Webcam.set({
+    width:350,
+    height:300,
+    image_format:"png",
+    png_quality:90
+});
+camera=document.getElementById("camera");
+Webcam.attach("#camera");
+
+
+function take_snapshot()
+{
+   Webcam.snap(function(data_uri)
+   {
+       document.getElementById("result").innerHTML='<img id="captured_image" src="'+data_uri+'"/>'; 
+   });
+   
+}
+classifier = ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/model.json",modelLoaded);
+function modelLoaded()
+{
+    console.log("model Loaded");
+}
